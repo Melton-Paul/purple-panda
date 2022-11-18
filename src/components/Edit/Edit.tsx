158a019e00f51238346a6da9f7abb299b75048d1
melton-paul <melton.paul@hotmail.com>
2022-11-18 17:37:55 -0600

    Allows for updating special of the day

--- src/components/Edit/Edit.tsx
+++ src/components/Edit/Edit.tsx
@@ -5,18 +5,18 @@ const Edit = () => {
   const [special, setSpecial] = React.useState({
     name: "",
     price: "",
-    img: null,
+    // img: null,
   });
 
   function handleChange(e: any) {
-    const [name, value] = e.target;
+    const { name, value } = e.target;
     setSpecial((prev) => ({ ...prev, [name]: value }));
   }
 
   function handleSubmit(e: any) {
     e.preventDefault();
     fetch("https://purplepanda-default-rtdb.firebaseio.com/special.json", {
-      method: "POST",
+      method: "PUT",
       body: JSON.stringify(special),
     });
   }
@@ -26,8 +26,8 @@ const Edit = () => {
       <h2 className="section-title">Edit Today's Special</h2>
       <form className={styles.form}>
         <input
-          onChange={handleChange}
           value={special.name}
+          onChange={handleChange}
           name="name"
           placeholder="Name"
           type="text"
